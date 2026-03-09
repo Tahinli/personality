ff12ae0527448ee00a09adb79b82e45e86e080cc
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2026-03-09 18:44:58 +0300

    feat: :spark: site tag changed to my name

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use dioxus::prelude::*;
 use personality::Route;
-use tracing::Level;
 
 fn main() {
-    // Init logger
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
     launch(app);
 }
 
 fn app() -> Element {
     rsx! {
+        document::Meta { name: "darkreader-lock" }
         link {
             rel: "stylesheet",
             style { {include_str!("../assets/main.css")} },
